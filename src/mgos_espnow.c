5f83772bf66cf701c630d4c78453bfda76c3c1df
Raymond Orihuela <79053004+orray@users.noreply.github.com>
2021-04-05 04:15:59 -0400

    Update mgos_espnow.c

--- src/mgos_espnow.c
+++ src/mgos_espnow.c
@@ -365,7 +365,7 @@ static esp_err_t mgos_espnow_internal_add_peer(struct mgos_espnow_peer *peer){
     }
     if(!mgos_sys_config_get_espnow_enable_broadcast()){
        uint8_t key[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
-       newpeer.lmk = key;
+       //newpeer.lmk = key;
        newpeer.encrypt = true;
     }else{
     newpeer.encrypt = false;
